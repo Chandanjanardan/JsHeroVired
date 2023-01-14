c243814f52aea2df9af1a70606176567d89ddb37
Chandan Janardan <chandanjanardan@gmail.com>
2023-01-14 17:38:01 +0530

    log

--- 18thDecJs/script.js
+++ 18thDecJs/script.js
@@ -6,20 +6,52 @@
 // div=fistNumber/secondNumber;
 
 // alert(result,sub,div)
-var speedInKmph=parseInt(prompt("Enter the value in KMPH"));
+// var speedInKmph=parseInt(prompt("Enter the value in KMPH"));
 
- mph=speedInKmph*.621
- meterPerSec=speedInKmph*.277
+//  mph=speedInKmph*.621
+//  meterPerSec=speedInKmph*.277
 
- document.write("Speed in Miles per hour"+ mph +"<br>"+"Speed in Meter per secomd"+meterPerSec)
-
- alert("Spped in Miles per hour"+ mph )
- alert("Speed in Meter per secomd"+meterPerSec) 
-
-
- console.log("Speed in Miles per hour"+ mph)
- console.log("Speed in meter per second"+ meterPerSec)
+//  document.writeln("Speed in Miles per hour"+ mph +"<br>"+"Speed in Meter per secomd"+meterPerSec)
  
+
+//  alert("Spped in Miles per hour"+ mph )
+//  alert("Speed in Meter per secomd"+meterPerSec) 
+
+
+//  console.log("Speed in Miles per hour"+ mph)
+//  console.log("Speed in meter per second"+ meterPerSec)
+// if blocks -----------------------------------------------------
+//  let a=4
+//  let b=10
+//  if (a>b){
+//     console.log("a is greater")
+//  }else{
+//     console.log("b is greater")
+//  }
+
+// odd even----------------------------------------------------- 
+
+ let number=7;
+ number%2==0?console.log("Number is even"):console.log("Number is odd")
+
+
+//  greates of three------------------------
+
+let valA=2
+let valB=9
+let valC=15
+
+if (valB>valA && valB>valC){
+    console.log("B is greatest")
+}
+else if(valA>valB && valA>valC){
+    console.log("A is greates")
+}else {console.log("C is greates")}
+
+
+
+
+
  
 
 
---
+++ 7thJan/script.js
@@ -0,0 +1,10 @@
+let number=555
+sum=0;
+while(number){
+    sum+=number%10;
+
+    value=Math.floor(number/10)
+    console.log(number)
+}
+console.log(sum)
+console.log(5)
