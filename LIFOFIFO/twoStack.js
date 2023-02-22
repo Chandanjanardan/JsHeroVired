e1259dcfe4b042cc434947f46991d672e067cd03
Chandan Janardan <chandanjanardan@gmail.com>
2023-02-22 22:45:31 +0530

    adding question 2

--- LIFOFIFO/twoStack.js
+++ LIFOFIFO/twoStack.js
@@ -29,3 +29,20 @@ queueLL.enqueue(4)
 queueLL.print()
 queueLL.dequeue()
 queueLL.print()
+
+// paranthesis problem
+
+function isBalanced(str) {
+    let stack = [];
+    for (let i = 0; i < str.length; i++) {
+      if (str[i] === '(') {
+        stack.push(str[i]); // if an opening parenthesis is encountered, push it onto the stack
+      } else if (str[i] === ')') {
+        if (stack.length === 0) { // if the stack is empty (meaning there is no corresponding opening parenthesis), return false
+          return false;
+        }
+        stack.pop(); // otherwise, pop the last opening parenthesis off the stack (since it matches the current closing parenthesis)
+      }
+    }
+    return stack.length === 0; // if the stack is empty after processing all parentheses, the string is balanced
+  }
