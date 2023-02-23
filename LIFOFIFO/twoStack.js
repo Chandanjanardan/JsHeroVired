ff71adebc3838df4dcd396f4c62e4dfe6d1ea206
Chandan Janardan <chandanjanardan@gmail.com>
2023-02-23 22:22:02 +0530

    adding factorial using recurrsion

--- LIFOFIFO/twoStack.js
+++ LIFOFIFO/twoStack.js
@@ -46,3 +46,48 @@ function isBalanced(str) {
     }
     return stack.length === 0; // if the stack is empty after processing all parentheses, the string is balanced
   }
+
+  class Stack {
+    constructor() {
+      this.stack = [];
+      this.minStack = [];
+    }
+  
+    push(val) {
+      this.stack.push(val);
+      if (this.minStack.length === 0 || val <= this.minStack[this.minStack.length - 1]) {
+        this.minStack.push(val);
+      }
+    }
+  
+    pop() {
+      if (this.stack.length === 0) {
+        return null;
+      }
+  
+      const val = this.stack.pop();
+  
+      if (val === this.minStack[this.minStack.length - 1]) {
+        this.minStack.pop();
+      }
+  
+      return val;
+    }
+  
+    top() {
+      if (this.stack.length === 0) {
+        return null;
+      }
+  
+      return this.stack[this.stack.length - 1];
+    }
+  
+    getMin() {
+      if (this.minStack.length === 0) {
+        return null;
+      }
+  
+      return this.minStack[this.minStack.length - 1];
+    }
+  }
+  
