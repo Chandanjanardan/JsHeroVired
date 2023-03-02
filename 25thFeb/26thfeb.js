2d315621696c6333115032cf4c8adb7eb86e9b42
Chandan Janardan <chandanjanardan@gmail.com>
2023-03-02 23:22:47 +0530

    adding double link list

--- 25thFeb/26thfeb.js
+++ 25thFeb/26thfeb.js
@@ -61,3 +61,23 @@ function greet_name(greeting,message,name){
 }
   
 greet_name(greet,'Welcome To GeeksForGeeks','JavaScript');
+
+const student = {
+
+    // data property
+    firstName: 'Monica',
+    
+    // accessor property(getter)
+    get getName() {
+        return this.firstName;
+    }
+};
+
+// accessing data property
+console.log(student.firstName); // Monica
+
+// accessing getter methods
+console.log(student.getName); // Monica
+
+// trying to access as a method
+console.log(student.getName()); // error
