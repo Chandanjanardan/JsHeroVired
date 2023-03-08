8dcd09745eb4ee5b1ffef36cb61f5793d8e5ef27
Chandan Janardan <chandanjanardan@gmail.com>
2023-03-08 23:00:26 +0530

    adding

--- 5thmarch/doubt.js
+++ 5thmarch/doubt.js
@@ -1,1 +1,24 @@
 console.log("Jello doubt")
+var ajaxRequest;  // The variable that makes Ajax possible!
+function ajaxFunction() {
+   try {
+      // Opera 8.0+, Firefox, Safari
+      ajaxRequest = new XMLHttpRequest();
+   } catch (e) {
+   
+      // Internet Explorer Browsers
+      try {
+         ajaxRequest = new ActiveXObject("Msxml2.XMLHTTP");
+      } catch (e) {
+      
+         try {
+            ajaxRequest = new ActiveXObject("Microsoft.XMLHTTP");
+         } catch (e) {
+      
+            // Something went wrong
+            alert("Your browser broke!");
+            return false;
+         }
+      }
+   }
+}
